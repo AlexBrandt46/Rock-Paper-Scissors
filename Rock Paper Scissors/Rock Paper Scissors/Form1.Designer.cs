b49741f4816d90ca739c4c3c01129791509dafce
AlexBrandt46 <ajb3155@gmail.com>
2018-01-21 17:38:23 -0500

    Initial Commit, and Created Main Menu

---
+++ Rock Paper Scissors/Rock Paper Scissors/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Rock_Paper_Scissors
+{
+    partial class frmLaunch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSinglePlayer = new System.Windows.Forms.Button();
+            this.btnTwoPlayer = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSinglePlayer
+            // 
+            this.btnSinglePlayer.Location = new System.Drawing.Point(14, 12);
+            this.btnSinglePlayer.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
+            this.btnSinglePlayer.Name = "btnSinglePlayer";
+            this.btnSinglePlayer.Size = new System.Drawing.Size(209, 54);
+            this.btnSinglePlayer.TabIndex = 0;
+            this.btnSinglePlayer.Text = "1 Player\r\nPlay Against a Computer-Controlled Bot\r\n";
+            this.btnSinglePlayer.UseVisualStyleBackColor = true;
+            // 
+            // btnTwoPlayer
+            // 
+            this.btnTwoPlayer.Location = new System.Drawing.Point(14, 72);
+            this.btnTwoPlayer.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
+            this.btnTwoPlayer.Name = "btnTwoPlayer";
+            this.btnTwoPlayer.Size = new System.Drawing.Size(209, 54);
+            this.btnTwoPlayer.TabIndex = 1;
+            this.btnTwoPlayer.Text = "2 Player\r\nPlay Against Another Human\r\n";
+            this.btnTwoPlayer.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(61, 132);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(109, 54);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // frmLaunch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(237, 197);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnTwoPlayer);
+            this.Controls.Add(this.btnSinglePlayer);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(253, 236);
+            this.MinimumSize = new System.Drawing.Size(253, 236);
+            this.Name = "frmLaunch";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main Menu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSinglePlayer;
+        private System.Windows.Forms.Button btnTwoPlayer;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+

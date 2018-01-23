fa87f90abf2145341b942fbdc2a6d6e7ee247731
AlexBrandt46 <ajb3155@gmail.com>
2018-01-22 20:30:36 -0500

    Starter GUI for player's choice, and added a groupbox to the launch menu to determine the max amount of games

--- Rock Paper Scissors/Rock Paper Scissors/Form1.Designer.cs
+++ Rock Paper Scissors/Rock Paper Scissors/Form1.Designer.cs
@@ -31,6 +31,11 @@
             this.btnSinglePlayer = new System.Windows.Forms.Button();
             this.btnTwoPlayer = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
+            this.grpBestOf = new System.Windows.Forms.GroupBox();
+            this.btn3Games = new System.Windows.Forms.RadioButton();
+            this.btn5 = new System.Windows.Forms.RadioButton();
+            this.btn7 = new System.Windows.Forms.RadioButton();
+            this.grpBestOf.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnSinglePlayer
@@ -55,7 +60,7 @@
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(61, 132);
+            this.btnExit.Location = new System.Drawing.Point(59, 194);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(109, 54);
             this.btnExit.TabIndex = 2;
@@ -63,21 +68,69 @@
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
+            // grpBestOf
+            // 
+            this.grpBestOf.Controls.Add(this.btn7);
+            this.grpBestOf.Controls.Add(this.btn5);
+            this.grpBestOf.Controls.Add(this.btn3Games);
+            this.grpBestOf.Location = new System.Drawing.Point(14, 138);
+            this.grpBestOf.Name = "grpBestOf";
+            this.grpBestOf.Size = new System.Drawing.Size(209, 50);
+            this.grpBestOf.TabIndex = 3;
+            this.grpBestOf.TabStop = false;
+            this.grpBestOf.Text = "Best Of:";
+            // 
+            // btn3Games
+            // 
+            this.btn3Games.AutoSize = true;
+            this.btn3Games.Location = new System.Drawing.Point(26, 19);
+            this.btn3Games.Name = "btn3Games";
+            this.btn3Games.Size = new System.Drawing.Size(31, 17);
+            this.btn3Games.TabIndex = 0;
+            this.btn3Games.TabStop = true;
+            this.btn3Games.Text = "3";
+            this.btn3Games.UseVisualStyleBackColor = true;
+            // 
+            // btn5
+            // 
+            this.btn5.AutoSize = true;
+            this.btn5.Location = new System.Drawing.Point(86, 19);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(31, 17);
+            this.btn5.TabIndex = 1;
+            this.btn5.TabStop = true;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            // 
+            // btn7
+            // 
+            this.btn7.AutoSize = true;
+            this.btn7.Location = new System.Drawing.Point(153, 19);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(31, 17);
+            this.btn7.TabIndex = 2;
+            this.btn7.TabStop = true;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            // 
             // frmLaunch
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(237, 197);
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.ClientSize = new System.Drawing.Size(237, 260);
+            this.Controls.Add(this.grpBestOf);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnTwoPlayer);
             this.Controls.Add(this.btnSinglePlayer);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(253, 236);
             this.MinimumSize = new System.Drawing.Size(253, 236);
             this.Name = "frmLaunch";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Main Menu";
+            this.grpBestOf.ResumeLayout(false);
+            this.grpBestOf.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -87,6 +140,10 @@
         private System.Windows.Forms.Button btnSinglePlayer;
         private System.Windows.Forms.Button btnTwoPlayer;
         private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.GroupBox grpBestOf;
+        private System.Windows.Forms.RadioButton btn7;
+        private System.Windows.Forms.RadioButton btn5;
+        private System.Windows.Forms.RadioButton btn3Games;
     }
 }
 
---
+++ Rock Paper Scissors/Rock Paper Scissors/PlayerChoice.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Rock_Paper_Scissors
+{
+    partial class PlayerChoice
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnRock = new System.Windows.Forms.RadioButton();
+            this.btnPaper = new System.Windows.Forms.RadioButton();
+            this.btnScissors = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnScissors);
+            this.groupBox1.Controls.Add(this.btnPaper);
+            this.groupBox1.Controls.Add(this.btnRock);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(192, 213);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Your Choice:";
+            // 
+            // btnRock
+            // 
+            this.btnRock.AutoSize = true;
+            this.btnRock.Location = new System.Drawing.Point(7, 20);
+            this.btnRock.Name = "btnRock";
+            this.btnRock.Size = new System.Drawing.Size(14, 13);
+            this.btnRock.TabIndex = 0;
+            this.btnRock.TabStop = true;
+            this.btnRock.UseVisualStyleBackColor = true;
+            // 
+            // btnPaper
+            // 
+            this.btnPaper.AutoSize = true;
+            this.btnPaper.Location = new System.Drawing.Point(6, 93);
+            this.btnPaper.Name = "btnPaper";
+            this.btnPaper.Size = new System.Drawing.Size(14, 13);
+            this.btnPaper.TabIndex = 1;
+            this.btnPaper.TabStop = true;
+            this.btnPaper.UseVisualStyleBackColor = true;
+            // 
+            // btnScissors
+            // 
+            this.btnScissors.AutoSize = true;
+            this.btnScissors.Location = new System.Drawing.Point(6, 180);
+            this.btnScissors.Name = "btnScissors";
+            this.btnScissors.Size = new System.Drawing.Size(14, 13);
+            this.btnScissors.TabIndex = 2;
+            this.btnScissors.TabStop = true;
+            this.btnScissors.UseVisualStyleBackColor = true;
+            // 
+            // PlayerChoice
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(219, 386);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "PlayerChoice";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Player Choice";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton btnScissors;
+        private System.Windows.Forms.RadioButton btnPaper;
+        private System.Windows.Forms.RadioButton btnRock;
+    }
+}

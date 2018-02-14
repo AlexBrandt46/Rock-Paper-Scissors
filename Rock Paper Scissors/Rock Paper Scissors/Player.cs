219f9927c64e45683c84cb364ae1a8c5757eb5ee
AlexBrandt46 <ajb3155@gmail.com>
2018-02-14 17:15:40 -0500

    Created the Player class

---
+++ Rock Paper Scissors/Rock Paper Scissors/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock_Paper_Scissors
+{
+    class Player
+    {
+
+        private int playerNumber;
+        public enum Choice
+        {
+            ROCK,
+            PAPER,
+            SCISSORS
+        }
+
+        public Player(int num)
+        {
+
+            playerNumber = num;
+
+        }
+
+        public int PlayerNumber {
+            get {
+                return playerNumber;
+            }
+        }
+
+        public Choice PlayerChoice {
+            get;
+            set;
+        }
+    }
+}

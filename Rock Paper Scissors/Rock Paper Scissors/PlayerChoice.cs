1ee734203058a69d31fdf76e53202f6d87c830b3
AlexBrandt46 <ajb3155@gmail.com>
2018-02-14 18:12:34 -0500

    Added signature for method to handle the continue, added code to change text in header of GUI, and changed formal method name in signature

--- Rock Paper Scissors/Rock Paper Scissors/PlayerChoice.cs
+++ Rock Paper Scissors/Rock Paper Scissors/PlayerChoice.cs
@@ -12,12 +12,21 @@ namespace Rock_Paper_Scissors
 {
     public partial class PlayerChoice : Form
     {
-        public PlayerChoice(Player playerOne)
+        public PlayerChoice(Player player)
         {
+
             InitializeComponent();
+
+            this.Text = "Player " + player.PlayerNumber + " Choice";
+
         }
 
         private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnContinue_Click(object sender, EventArgs e)
         {
 
         }

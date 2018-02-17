6078b96332639863b0081f9abf5b1aea503042e2
AlexBrandt46 <ajb3155@gmail.com>
2018-02-17 12:38:34 -0500

    Began formatting of Results page

--- Rock Paper Scissors/Rock Paper Scissors/Results.Designer.cs
+++ Rock Paper Scissors/Rock Paper Scissors/Results.Designer.cs
@@ -28,11 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblPlayerOne = new System.Windows.Forms.Label();
+            this.lblPlayerTwo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblPlayerOne
+            // 
+            this.lblPlayerOne.AutoSize = true;
+            this.lblPlayerOne.Font = new System.Drawing.Font("Arial", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayerOne.Location = new System.Drawing.Point(13, 13);
+            this.lblPlayerOne.Name = "lblPlayerOne";
+            this.lblPlayerOne.Size = new System.Drawing.Size(150, 32);
+            this.lblPlayerOne.TabIndex = 0;
+            this.lblPlayerOne.Text = "Player One";
+            // 
+            // lblPlayerTwo
+            // 
+            this.lblPlayerTwo.AutoSize = true;
+            this.lblPlayerTwo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayerTwo.Location = new System.Drawing.Point(329, 9);
+            this.lblPlayerTwo.Name = "lblPlayerTwo";
+            this.lblPlayerTwo.Size = new System.Drawing.Size(150, 31);
+            this.lblPlayerTwo.TabIndex = 1;
+            this.lblPlayerTwo.Text = "Player Two";
+            // 
+            // Results
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 377);
+            this.Controls.Add(this.lblPlayerTwo);
+            this.Controls.Add(this.lblPlayerOne);
+            this.Name = "Results";
             this.Text = "Results";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblPlayerOne;
+        private System.Windows.Forms.Label lblPlayerTwo;
     }
 }

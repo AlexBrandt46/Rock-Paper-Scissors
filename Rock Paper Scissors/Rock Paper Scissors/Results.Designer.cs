4f32db2b93f990f9ac9857cbed0abdd61566fce9
AlexBrandt46 <ajb3155@gmail.com>
2018-02-23 15:45:05 -0500

    Added picture boxes and labels for each player's score

--- Rock Paper Scissors/Rock Paper Scissors/Results.Designer.cs
+++ Rock Paper Scissors/Rock Paper Scissors/Results.Designer.cs
@@ -30,15 +30,23 @@
         {
             this.lblPlayerOne = new System.Windows.Forms.Label();
             this.lblPlayerTwo = new System.Windows.Forms.Label();
+            this.pcboxPlayerOne = new System.Windows.Forms.PictureBox();
+            this.pcBoxPlayerTwo = new System.Windows.Forms.PictureBox();
+            this.lblPlayer1Score = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblPlayer2Score = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pcboxPlayerOne)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcBoxPlayerTwo)).BeginInit();
             this.SuspendLayout();
             // 
             // lblPlayerOne
             // 
             this.lblPlayerOne.AutoSize = true;
-            this.lblPlayerOne.Font = new System.Drawing.Font("Arial", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayerOne.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblPlayerOne.Location = new System.Drawing.Point(13, 13);
             this.lblPlayerOne.Name = "lblPlayerOne";
-            this.lblPlayerOne.Size = new System.Drawing.Size(150, 32);
+            this.lblPlayerOne.Size = new System.Drawing.Size(149, 31);
             this.lblPlayerOne.TabIndex = 0;
             this.lblPlayerOne.Text = "Player One";
             // 
@@ -52,15 +60,79 @@
             this.lblPlayerTwo.TabIndex = 1;
             this.lblPlayerTwo.Text = "Player Two";
             // 
+            // pcboxPlayerOne
+            // 
+            this.pcboxPlayerOne.Location = new System.Drawing.Point(13, 61);
+            this.pcboxPlayerOne.Name = "pcboxPlayerOne";
+            this.pcboxPlayerOne.Size = new System.Drawing.Size(150, 125);
+            this.pcboxPlayerOne.TabIndex = 2;
+            this.pcboxPlayerOne.TabStop = false;
+            // 
+            // pcBoxPlayerTwo
+            // 
+            this.pcBoxPlayerTwo.Location = new System.Drawing.Point(329, 61);
+            this.pcBoxPlayerTwo.Name = "pcBoxPlayerTwo";
+            this.pcBoxPlayerTwo.Size = new System.Drawing.Size(150, 125);
+            this.pcBoxPlayerTwo.TabIndex = 3;
+            this.pcBoxPlayerTwo.TabStop = false;
+            // 
+            // lblPlayer1Score
+            // 
+            this.lblPlayer1Score.AutoSize = true;
+            this.lblPlayer1Score.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer1Score.Location = new System.Drawing.Point(56, 230);
+            this.lblPlayer1Score.Name = "lblPlayer1Score";
+            this.lblPlayer1Score.Size = new System.Drawing.Size(0, 54);
+            this.lblPlayer1Score.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(7, 199);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Player One Score";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(324, 199);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(167, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Player Two Score";
+            // 
+            // lblPlayer2Score
+            // 
+            this.lblPlayer2Score.AutoSize = true;
+            this.lblPlayer2Score.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer2Score.Location = new System.Drawing.Point(382, 230);
+            this.lblPlayer2Score.Name = "lblPlayer2Score";
+            this.lblPlayer2Score.Size = new System.Drawing.Size(0, 54);
+            this.lblPlayer2Score.TabIndex = 7;
+            // 
             // Results
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(491, 377);
+            this.Controls.Add(this.lblPlayer2Score);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblPlayer1Score);
+            this.Controls.Add(this.pcBoxPlayerTwo);
+            this.Controls.Add(this.pcboxPlayerOne);
             this.Controls.Add(this.lblPlayerTwo);
             this.Controls.Add(this.lblPlayerOne);
             this.Name = "Results";
             this.Text = "Results";
+            ((System.ComponentModel.ISupportInitialize)(this.pcboxPlayerOne)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcBoxPlayerTwo)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -70,5 +142,11 @@
 
         private System.Windows.Forms.Label lblPlayerOne;
         private System.Windows.Forms.Label lblPlayerTwo;
+        private System.Windows.Forms.PictureBox pcboxPlayerOne;
+        private System.Windows.Forms.PictureBox pcBoxPlayerTwo;
+        private System.Windows.Forms.Label lblPlayer1Score;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblPlayer2Score;
     }
 }

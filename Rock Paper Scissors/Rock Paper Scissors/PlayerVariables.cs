d393d4f8608c4eb94635bfdfc8dcd4639b0ceb4b
AlexBrandt46 <ajb3155@gmail.com>
2018-02-23 15:42:54 -0500

    Created singleton class to store global variables

---
+++ Rock Paper Scissors/Rock Paper Scissors/PlayerVariables.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock_Paper_Scissors
+{
+    class PlayerVariables
+    {
+
+        public static int rounds; //maximum amount of rounds that can be played
+        public static int playerOneScore = 0;
+        public static int playerTwoScore = 0;
+        public static Player.Choice[] choices = new Player.Choice[2]; //each player's choices
+
+
+    }
+}

5f7dd15e3e4a1ea623781eacb9f6419f0c25b6e3
AlexBrandt46 <ajb3155@gmail.com>
2018-03-04 19:40:06 -0500

    Finished formatting GUI

--- Rock Paper Scissors/Rock Paper Scissors/WinPage.Designer.cs
+++ Rock Paper Scissors/Rock Paper Scissors/WinPage.Designer.cs
@@ -28,24 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblWinner = new System.Windows.Forms.Label();
+            this.lblScores = new System.Windows.Forms.Label();
+            this.lblPlayerOne = new System.Windows.Forms.Label();
+            this.lblPlayerTwo = new System.Windows.Forms.Label();
+            this.lblPlayer2Score = new System.Windows.Forms.Label();
+            this.lblPlayer1Score = new System.Windows.Forms.Label();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // lblWinner
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(158, 135);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.lblWinner.AutoSize = true;
+            this.lblWinner.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWinner.Location = new System.Drawing.Point(72, 9);
+            this.lblWinner.Name = "lblWinner";
+            this.lblWinner.Size = new System.Drawing.Size(240, 31);
+            this.lblWinner.TabIndex = 0;
+            this.lblWinner.Text = "Player One Wins!";
+            this.lblWinner.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblScores
+            // 
+            this.lblScores.AutoSize = true;
+            this.lblScores.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScores.Location = new System.Drawing.Point(105, 61);
+            this.lblScores.Name = "lblScores";
+            this.lblScores.Size = new System.Drawing.Size(164, 31);
+            this.lblScores.TabIndex = 1;
+            this.lblScores.Text = "Scoreboard";
+            // 
+            // lblPlayerOne
+            // 
+            this.lblPlayerOne.AutoSize = true;
+            this.lblPlayerOne.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayerOne.Location = new System.Drawing.Point(2, 109);
+            this.lblPlayerOne.Margin = new System.Windows.Forms.Padding(100, 0, 3, 0);
+            this.lblPlayerOne.Name = "lblPlayerOne";
+            this.lblPlayerOne.Padding = new System.Windows.Forms.Padding(50, 0, 0, 0);
+            this.lblPlayerOne.Size = new System.Drawing.Size(160, 25);
+            this.lblPlayerOne.TabIndex = 2;
+            this.lblPlayerOne.Text = "Player One";
+            // 
+            // lblPlayerTwo
+            // 
+            this.lblPlayerTwo.AutoSize = true;
+            this.lblPlayerTwo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayerTwo.Location = new System.Drawing.Point(230, 109);
+            this.lblPlayerTwo.Name = "lblPlayerTwo";
+            this.lblPlayerTwo.Padding = new System.Windows.Forms.Padding(0, 0, 50, 0);
+            this.lblPlayerTwo.Size = new System.Drawing.Size(160, 25);
+            this.lblPlayerTwo.TabIndex = 3;
+            this.lblPlayerTwo.Text = "Player Two";
+            // 
+            // lblPlayer2Score
+            // 
+            this.lblPlayer2Score.AutoSize = true;
+            this.lblPlayer2Score.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer2Score.Location = new System.Drawing.Point(265, 144);
+            this.lblPlayer2Score.Name = "lblPlayer2Score";
+            this.lblPlayer2Score.Size = new System.Drawing.Size(24, 25);
+            this.lblPlayer2Score.TabIndex = 4;
+            this.lblPlayer2Score.Text = "0";
+            // 
+            // lblPlayer1Score
+            // 
+            this.lblPlayer1Score.AutoSize = true;
+            this.lblPlayer1Score.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer1Score.Location = new System.Drawing.Point(89, 144);
+            this.lblPlayer1Score.Name = "lblPlayer1Score";
+            this.lblPlayer1Score.Size = new System.Drawing.Size(24, 25);
+            this.lblPlayer1Score.TabIndex = 5;
+            this.lblPlayer1Score.Text = "0";
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(78, 230);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(75, 23);
+            this.btnNew.TabIndex = 6;
+            this.btnNew.Text = "button1";
+            this.btnNew.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(237, 230);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "button2";
+            this.btnExit.UseVisualStyleBackColor = true;
             // 
             // WinPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
             this.ClientSize = new System.Drawing.Size(393, 276);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.lblPlayer1Score);
+            this.Controls.Add(this.lblPlayer2Score);
+            this.Controls.Add(this.lblPlayerTwo);
+            this.Controls.Add(this.lblPlayerOne);
+            this.Controls.Add(this.lblScores);
+            this.Controls.Add(this.lblWinner);
             this.Name = "WinPage";
             this.Text = "WinPage";
             this.ResumeLayout(false);
@@ -55,6 +143,13 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblWinner;
+        private System.Windows.Forms.Label lblScores;
+        private System.Windows.Forms.Label lblPlayerOne;
+        private System.Windows.Forms.Label lblPlayerTwo;
+        private System.Windows.Forms.Label lblPlayer2Score;
+        private System.Windows.Forms.Label lblPlayer1Score;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnExit;
     }
 }
